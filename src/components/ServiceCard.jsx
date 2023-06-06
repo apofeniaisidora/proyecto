c91bf9d4bbb3d2502a20d9a4d93b60c874a86b3b
apofeniaisidora <isidoracorreaflores@gmail.com>
2023-06-06 13:56:49 -0400

    arreglemensaje

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import "../index.css";
 import { FavoritosContext } from "../context/FavoritosContext";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const ServiceCard = ({ servicio }) => {
   const { addFavoritos, favoritos } = useContext(FavoritosContext);
@@ -9,16 +9,20 @@ const ServiceCard = ({ servicio }) => {
   return (
     <article className="card">
       <img src={servicio.img} alt="" className="imagencard" />
+      <br></br>
       <h2>{servicio.title}</h2>
       <p>{servicio.description}</p>
       <h3>{servicio.price}</h3>
       <br></br>
-      <button className="green boton"><NavLink to="/mensaje">Contactar</NavLink></button>
+      <button className="green boton">
+        <Link to="/mensaje">Contactar</Link>
+      </button>
       <br></br>
-      <button className="green boton botonguardar" onClick={() => addFavoritos(servicio)}
-      disabled= {
-        favoritos.some(item => item.id === servicio.id)
-      }>
+      <button
+        className="green boton botonguardar"
+        onClick={() => addFavoritos(servicio)}
+        disabled={favoritos.some((item) => item.id === servicio.id)}
+      >
         Guardar
       </button>
     </article>
--- src/views/Favoritos.jsx
+++ src/views/Favoritos.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { FavoritosContext } from "../context/FavoritosContext";
+import { Link } from "react-router-dom";
 
 const Favoritos = () => {
   const { favoritos, deleteFavoritos } = useContext(FavoritosContext);
@@ -11,7 +12,15 @@ const Favoritos = () => {
         <article key={item.id}>
           <h1>{item.title}</h1>
           <p>{item.user}</p>
-          <button onClick={()=> deleteFavoritos(item.id)} className="boton green">Eliminar</button>
+          <button className="green boton">
+            <Link to="/mensaje">Contactar</Link>
+          </button>
+          <button
+            onClick={() => deleteFavoritos(item.id)}
+            className="boton botonguardar"
+          >
+            Eliminar
+          </button>
         </article>
       ))}
       {favoritos.length === 0 && <h2>No existen favoritos</h2>}
